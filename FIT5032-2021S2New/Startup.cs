1a161aba9dc27895bcbcf1c386535f8509617e71
JWenJie <wjin0011@student.monash.edu>
2021-09-16 14:24:26 +0800

    New first commit

---
+++ FIT5032-2021S2New/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_2021S2New.Startup))]
+namespace FIT5032_2021S2New
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
